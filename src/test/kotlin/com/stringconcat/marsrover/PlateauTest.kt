5377624d148713926ddb44c7689e3b37d78f73c8
ivanxalie <ivanxalie@gmail.com>
2024-09-14 23:52:45 +0300

    Tdd on chill: don't remember what I did, even though I was sober.

--- src/test/kotlin/com/stringconcat/marsrover/PlateauTest.kt
+++ src/test/kotlin/com/stringconcat/marsrover/PlateauTest.kt
@@ -1,42 +1,51 @@
 package com.stringconcat.marsrover
 
 
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertThrows
-import kotlin.test.assertFails
-import kotlin.test.assertFalse
-import kotlin.test.assertTrue
 
 class PlateauTest {
     private val plateau = Plateau()
 
     @Test
     fun `plateau with negative coordinates cannot be created`() {
-        assertThrows<IllegalArgumentException> { Plateau(-1, -1) }
-        assertThrows<IllegalArgumentException> { Plateau(0, 0) }
+        shouldThrow<IllegalArgumentException> { Plateau(-1, -1) }
+        shouldThrow<IllegalArgumentException> { Plateau(0, 0)  }
     }
 
     @Test
     fun `land on position beyond limitations are prohibited` () {
-        val plateau = Plateau()
-
-        assertThrows<PositionOutsideBound> { plateau.land(Coordinate(6, 0)) }
-        assertThrows<PositionOutsideBound> { plateau.land(Coordinate(0, 6)) }
-        assertThrows<PositionOutsideBound> { plateau.land(Coordinate(6, 6)) }
+        shouldThrow<PositionOutsideBound> { plateau.land(Coordinate(6, 0)) }
+        shouldThrow<PositionOutsideBound> { plateau.land(Coordinate(0, 6)) }
+        shouldThrow<PositionOutsideBound> { plateau.land(Coordinate(6, 6)) }
     }
 
     @Test
     fun `land on the same position more than once at the same time are prohibited`() {
         plateau.land(Coordinate.zero())
-        assertThrows<PositionAlreadyTaken> { plateau.land(Coordinate.zero()) }
+
+        shouldThrow<PositionAlreadyTaken> { plateau.land(Coordinate.zero()) }
     }
 
     @Test
     fun `move right take new position on the right`() {
         plateau.land(Coordinate.zero())
-        plateau.move(Coordinate.zero(), Coordinate.zero().incX())
+        val moveResult = plateau.move(Coordinate.zero(), Coordinate.zero().incX())
 
-        assertTrue { plateau.containsCoordinate(Coordinate.zero().incX()) }
-        assertFalse { plateau.containsCoordinate(Coordinate.zero()) }
+        plateau.containsCoordinate(Coordinate.zero().incX()) shouldBe true
+        plateau.containsCoordinate(Coordinate.zero()) shouldBe false
+    }
+
+    @Test
+    fun `move move left take new position on the left`() {
+        val source = Coordinate(1, 1)
+        val destination = source.decX()
+
+        plateau.land(source)
+        plateau.move(source, destination)
+
+        plateau.containsCoordinate(destination) shouldBe true
+        plateau.containsCoordinate(source) shouldBe false
     }
 }
